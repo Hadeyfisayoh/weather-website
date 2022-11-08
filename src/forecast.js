b60caa161bb90f17c979a74b71f534c3ea213d0d
Hardey <adebisienoch5@gmail.com>
2022-11-08 23:26:45 +0100

    adding humidity

--- src/forecast.js
+++ src/forecast.js
@@ -7,7 +7,7 @@ const forecast = (latitude, longitude, callback) => {
       } else if (body.error){
         callback('Unable to find location')
       }else {
-        callback(undefined, body.current.weather_descriptions[0] +'. It is currently '+ body.current.temperature + ' feels like '+ body.current.feelslike)
+        callback(undefined, body.current.weather_descriptions[0] +'. It is currently '+ body.current.temperature + ' feels like '+ body.current.feelslike + '. The humidity is ' +  body.current.humidity +'%')
       }
     })
 
